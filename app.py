d9b0edeebc8bc3d56952a868bef051e316de65dc
LimChangHun81 <81498418+LimChangHun81@users.noreply.github.com>
2025-02-01 14:09:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
-st.title("테스트 앱")
-st.write("Hello, Streamlit!@@@jnnjinjuinu  jju")
+# st.title("테스트 앱")
+# st.write("Hello, Streamlit!@@@jnnjinjuinu  jju")
+st.title("Streamlit 기초 예제")
+st.header("헤더입니다.")
+st.subheader("서브헤더입니다.")
+st.write("안녕하세요! Streamlit을 사용해 보세요.")
+st.markdown("**마크다운**을 사용할 수도 있습니다.")
